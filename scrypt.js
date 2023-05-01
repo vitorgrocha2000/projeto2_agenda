275b21ed657e8011199a5876cda4aee0d257bf3a
Vitor Rocha <vitorrocha-2000@hotmail.com>
2023-05-02 00:20:41 +0100

    modify scrypt.js and style.css

--- scrypt.js
+++ scrypt.js
@@ -8,6 +8,9 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     adiciona_linha()
+    atualiza_tabela()
+    // imprime_tabela()
+    atualiza_quantidade()
 
 })
 
@@ -19,17 +22,46 @@ function adiciona_linha() {
     if (contatos.includes(nome_contato.value)) {
         alert(`O contato "${nome_contato.value}" já foi inserida`)
     } else {
-        numeros.push(numero_contato.value);
-        contatos.push(nome_contato.value);
+        const numero_contato = document.getElementById("numero_contato");
 
-        linha += `<tr>`
-        linha += `<td>${nome_contato.value}</td>`
-        linha += `<td>${numero_contato.value}</td>`
-        linha += `<td>${numero_contato.value >= nota_min ? img_aprovado : img_reprovado}</td>`
-        // linha += `<td>${botao_remove}</td>`
-        linha += `</tr>`
+        if (!numero_contato.value.startsWith("+")) {
+            alert("Por favor, adicione o código do país com o sinal de +");
+            return false;
+        } else {
+            numeros.push(numero_contato.value);
+            contatos.push(nome_contato.value);
+
+            linha += `<tr>`
+            linha += `<td>${nome_contato.value}</td>`
+            linha += `<td><a id="ligue" href="tel:${numero_contato.value}">${numero_contato.value}</a></td>`
+            // linha += `<td>${numero_contato.value}</td>`
+            linha += `</tr>`
+
+            nome_contato.value = ''
+            numero_contato.value = ''
+        }
     }
-
-    nome_contato = ''
-    numero_contato = ''
 }
+
+function atualiza_tabela() {
+    const table_body = document.querySelector('tbody')
+    table_body.innerHTML = linha
+}
+
+
+function atualiza_quantidade() {
+
+    let quantidade = '<tr>'
+    quantidade += `<td>Quantidade de contatos:</td>`
+    quantidade += `<td>${numeros.length}</td>`
+    quantidade += `</tr>`
+
+    const quantidade_contatos = document.querySelector('tfoot')
+    quantidade_contatos.innerHTML = quantidade
+}
+
+// function imprime_tabela() {
+//     console.log(contatos)
+//     console.log(numeros)
+//     console.log(linha)
+// }
